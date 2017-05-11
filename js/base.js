ae2589f271a7d595db75e252cee623546bab876e
huyanlong <49175347@qq.com>
2017-05-11 17:26:51 +0800

    五月十一日下班提交

---
+++ js/base.js
@@ -0,0 +1,10 @@
+(function(){
+    var logoSearch=document.getElementById("logo_search");
+        logoSearch.onchange=function(){
+            if (logoSearch.value!="") {
+                logoSearch.style.background="none";
+            }
+        }
+
+
+})();
